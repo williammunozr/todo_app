9dcdb86881b67d7e58eb72f473b953273b39e116
william <william.munozr@gmail.com>
2023-05-07 21:59:35 -0500

    add structuring the project

---
+++ src/to_do/enums.rs
@@ -0,0 +1,13 @@
+pub enum TaskStatus {
+    DONE,
+    PENDING
+}
+
+impl TaskStatus {
+    pub fn stringify(&self) -> String {
+        match &self {
+            &Self::DONE => {"DONE".to_string()},
+            &Self::PENDING => {"PENDING".to_string()}
+        }
+    }
+}
