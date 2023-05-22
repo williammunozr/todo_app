c3d9fd7a70dad10bc4f12968495da9f0f8bd3433
william <william@godeveloper.io>
2023-05-21 22:48:30 -0500

    Implementing processes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rust Todo Application
+
+## How to run the application
+
+```commandline
+cargo run create washing
+cargo run get washing
+cargo run edit washing (set to done)
+```
--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,16 @@ use state::read_file;
 use to_do::to_do_factory;
 use to_do::enums::TaskStatus;
 use processes::process_input;
-/*use to_do::ItemTypes;
-use crate::to_do::traits::get::Get;
-use crate::to_do::traits::delete::Delete;
-use crate::to_do::traits::edit::Edit;*/
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command: &String = &args[1];
     let title: &String = &args[2];
-
     let state: Map<String, Value> = read_file("./state.json");
     let status: String;
     match &state.get(*&title) {
         Some(result) => {
-            result.to_string().replace('\"', "");
+            status = result.to_string().replace('\"', "");
         }
         None => {
             status = "pending".to_owned();
